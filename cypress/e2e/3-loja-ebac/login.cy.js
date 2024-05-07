1da55a38438de2005e1fcbe6d5f16a8f2e7ec3ed
marcia yurie yano <83242742+marciayano@users.noreply.github.com>
2024-05-06 21:12:18 -0300

    testes negativos e hooks

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -2,13 +2,34 @@
 
 describe('Funcionalidade Login', () => {
 
-  it('Deve fazer login com sucesso',() => {
+  beforeEach(() => {
     cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+  });
+
+  afterEach(() => {
+    cy.screenshot()
+  });
+
+  it('Deve fazer login com sucesso',() => {
     cy.get('#username').type('marcia@teste.com.br')
     cy.get('#password').type('teste@123')
     cy.get('.woocommerce-form > .button').click()
-
     cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain','Olá, marcia (não é marcia? Sair)')
   })
+
+  it('Deve exibir mensagem de erro com usuário invalido', () => {
+    cy.get('#username').type('invalido')
+    cy.get('#password').type('teste@123')
+    cy.get('.woocommerce-form > .button').click()
+    //cy.get('.woocommerce-error').should('contain','Erro: O usuário invalido não está registrado neste site.')
+    cy.get('.woocommerce-error').should('exist')
+  });
+
+  it('Deve exibir mensagem de erro com senha invalida', () => {
+    cy.get('#username').type('marcia@teste.com.br')
+    cy.get('#password').type('invalido')
+    cy.get('.woocommerce-form > .button').click()
+    cy.get('.woocommerce-error').should('exist')
+  });
 
 })
