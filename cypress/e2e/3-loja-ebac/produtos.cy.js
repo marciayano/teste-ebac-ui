e11ad6f0fe411bcb4174ca55ed8f715213b69fa8
marcia yurie yano <83242742+marciayano@users.noreply.github.com>
2024-05-07 20:53:16 -0300

    page object completo

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -25,10 +25,24 @@ describe('Funcionalidade Produtos', () => {
   });
 
   it('Deve visitar a pagina do produto', () => {
-    
+    produtosPage.visitarProduto('aero-daily-fitness-tee')
+    cy.get('.product_title').should('contain','Aero Daily Fitness Tee')
   });
 
   it('Deve adicionar um produto ao carrinho', () => {
+    let qtd = 7
+    produtosPage.buscarProduto('Aether Gym Pant')
+    produtosPage.addProdutoCarrinho('33','Brown',qtd)
+    cy.get('.woocommerce-message').should('contain', qtd + ' × “Aether Gym Pant” foram adicionados no seu carrinho.')
+  });
+
+  it.only('Deve adicionar um produto ao carrinho buscando da massa de dados', () => {
+    cy.fixture('produtos').then(dados => {
+      produtosPage.buscarProduto(dados[1].nomeProduto)
+      produtosPage.addProdutoCarrinho(dados[1].tamanho, dados[1].cor,dados[1].quantidade)
+      cy.get('.woocommerce-message').should('contain', dados[1].nomeProduto)
+    })
+    
     
   });
   
--- cypress/support/page-objects/produtos.page.js
+++ cypress/support/page-objects/produtos.page.js
@@ -13,12 +13,17 @@ class ProdutosPage{
     cy.get('.product-block').contains(nomeProduto).click()
   }
 
-  visitarProduto(){
-
+  visitarProduto(nomeProduto){
+    //cy.visit(`produtos/${nomeProduto}`)
+    const urlFormatada = nomeProduto.replace(/ /g, '-')
+    cy.visit(`produtos/${urlFormatada}`)
   }
 
-  addProdutoCarrinho(){
-    
+  addProdutoCarrinho(tamanho, cor, quantidade){
+    cy.get('.button-variable-item-' + tamanho).click()
+    cy.get(`.button-variable-item-${cor}`).click()
+    cy.get('.input-text').clear().type(quantidade)
+    cy.get('.single_add_to_cart_button').click()
   }
 
 }
